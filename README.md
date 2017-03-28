6c258b75e9e757862b6bb2685c42f782e04dc78a
Robert Danitz <robert@advancedtelematic.com>
2017-03-28 16:00:30 +0200

    initial release

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # teleport
-a simple tool to jump around directories
+Lets you jump around directories by pre-defined mnemonics.
+Heavily inspired by https://github.com/bollu/teleport.
+
+## Dependencies
+- guile 2.0+
+- guile-dbi
+- guile-dbd-sqlite3
+
+## Setup
+Put the `teleport.sh` script somewhere in PATH, then add this to your profile:
+```sh
+function tp() {
+  $(teleport.sh "$@")
+}
+```
+
+Initialize the database:
+```sh
+tp --init
+```
+
+Mark the current working dir:
+```sh
+cd /some/long/path
+tp --add here be dragons
+```
+
+Jump around:
+```sh
+cd ~
+tp here be dragons
+```
---
+++ teleport.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/guile \
+-e main -s
+!#
+(use-modules (ice-9 match) (dbi dbi))
+
+(define db
+  (let ((xdg (getenv "XDG_CONFIG_HOME"))
+        (sub "sqlite3"))
+    (if xdg
+      (dbi-open sub (string-append xdg "/tp/db"))
+      (dbi-open sub (string-append (getenv "HOME") "/.tp/db")))))
+
+(define (init-db)
+  (dbi-query db "create table locations(mnemo varchar(64), target varchar(255), primary key(mnemo))"))
+
+(define (insert! mnemo target)
+  (let ((q (string-append "insert or replace into locations values ('" mnemo "', '" target "')")))
+    (dbi-query db q)))
+
+(define (query mnemo)
+  (let ((q (string-append "select target from locations where mnemo = '" mnemo "'")))
+    (dbi-query db q)
+    (match (dbi-get_row db)
+      (#f (display "target is not defined" (current-error-port)) #f)
+      (((_ . target)) target))))
+
+(define (find prefix)
+  (define (find')
+    (let ((r (dbi-get_row db)))
+      (when r
+        (display (cdr (car r)) (current-error-port))
+        (newline (current-error-port))
+        (find'))))
+  (let ((q (string-append "select mnemo from locations where mnemo like('" prefix "%')")))
+    (dbi-query db q)
+    (find')))
+
+(define (teleport target)
+  (if target
+    (display (string-append "cd " target))
+    (display "false")))
+
+(define (help)
+  (display "usage: tp {--init | --list mnemo-prefix | --add mnemo | mnemo}\n" (current-error-port)))
+
+(define (main args)
+  (match args
+    ((_) (help))
+    ((_ "--init") (init-db))
+    ((or (_ "-l" . prefix) (_ "--list" . prefix))
+      (find (string-join prefix)))
+    ((or (_ "-a" . mnemo) (_ "--add" . mnemo))
+      (insert! (string-join mnemo) (getcwd)))
+    ((_ . mnemo)
+      (teleport (query (string-join mnemo))))
+    (else (help))))
+
